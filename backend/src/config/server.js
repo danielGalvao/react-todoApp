0c3ff4a0c014b15a4ec3ad5e6d4cb769ec8dc401
Daniel Galvão <daniel.galvao@justdigital.com.br>
2017-03-08 09:40:21 -0300

    Finish config server and mongodb

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 const server = express();
 
-server.user(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
 server.listen(port, function(){
