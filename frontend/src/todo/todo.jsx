25e6e5c1cf88e91d90e1d9f1fb02b30e0e61c3b5
Daniel Galvão <galvaodev@gmail.com>
2017-03-12 18:00:01 -0300

    Add form and create components

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,29 @@ import TodoForm from './todoForm';
 import TodoList from './todoList';
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { description: '', list: [] };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({...this.state, description: e.target.value});
+  }
+
+  handleAdd() {
+
+  }
+
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro" />
-        <TodoForm />
+        <TodoForm description={this.state.description}
+          handleChange={this.handleChange}
+          handleAdd={this.handleAdd}/>
         <TodoList />
       </div>
     )
