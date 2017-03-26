3050d38d622263fed2fed881ddc7864394626ab6
Daniel Galvão <galvaodev@gmail.com>
2017-03-25 22:42:44 -0300

    Add middleware configo redux-promise

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-  description: 'Ler livro',
-  list: [{
-    _id: 1,
-    description: 'Pagar fatura',
-    done: true
-  },{
-    _id: 2,
-    description: 'Pagar conta',
-    done: false
-  },{
-    _id: 3,
-    description: 'Pagar aluguel',
-    done: false
-  }]
-}
+const INITIAL_STATE = { description: 'Ler livro', list: [] };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
